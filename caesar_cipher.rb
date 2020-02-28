ddf20769b897ee4535c18e194325523e66ab112c
Rachel Moser <moser.rachel@gmail.com>
2020-02-28 16:58:19 -0600

    finalized logic for cipher

--- caesar_cipher.rb
+++ caesar_cipher.rb
@@ -1,14 +1,20 @@
 def caesar_cipher(message, shift)
-    message.split('').each do |letter|
-        puts letter.ord
+    result = ""
+    message.each_char do |char|
+        # Modifies Uppercase
+        if char.ord >= 65 && char.ord <= 90
+            rotation = (((char.ord - 65) + shift) % 26) + 65
+            result += rotation.chr
+        # Modifies Lowercase
+        elsif char.ord >= 97 && char.ord <= 122
+            rotation = (((char.ord - 97) + shift) % 26) + 97
+            result += rotation.chr
+        # Keeps spaces & punctuation
+        else
+            result += char
+        end
     end
-    puts shift
+    puts result
 end
 
-caesar_cipher("AZaz", 1)
-# caesar_cipher("What a string!", 5) 
-# "Bmfy f xywnsl!"
-# a = 97
-# z = 122
-# A = 65
-# Z = 90
+caesar_cipher("What a string!", 5)
