e2fe7a513ff0770fff5981369dbf022087e394c0
Rachel Moser <moser.rachel@gmail.com>
2020-02-28 17:55:51 -0600

    start stock picker file

---
+++ stock_picker.rb
@@ -0,0 +1,10 @@
+def stock_picker array
+    array.each do |num|
+        puts num
+    end
+end
+
+stock_picker([17,3,6,9,15,8,6,1,10])
+
+# Need to find the largest number
+# Need to find the smallest number BEFORE the largest number
