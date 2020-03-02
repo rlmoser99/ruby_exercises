f23158567eb1d7fb67e71ae4eeac9c3ac7b2ddf2
Rachel Moser <moser.rachel@gmail.com>
2020-03-01 18:07:03 -0600

    start substring exercise

---
+++ substrings.rb
@@ -0,0 +1,8 @@
+dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
+
+def substrings (string, dictionary)
+    puts "string"
+    puts dictionary[0]
+end
+
+substrings("below", dictionary)
