9b6b60a24ee5cbb9ade95bb246660c35cff0da35
Rachel Moser <moser.rachel@gmail.com>
2020-03-01 21:42:34 -0600

    hash logic for substrings

--- substrings.rb
+++ substrings.rb
@@ -1,8 +1,14 @@
 dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
 
 def substrings (string, dictionary)
-    puts "string"
-    puts dictionary[0]
+    matches = {}
+    dictionary.each do |word|
+        if string.downcase.include?(word)
+            matches[word] = 1
+        end
+    end
+    puts matches
 end
 
-substrings("below", dictionary)
+substrings("Below low it sit", dictionary)
+# substrings("Howdy partner, sit down! How's it going?", dictionary)
