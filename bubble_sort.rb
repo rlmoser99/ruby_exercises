0cfc7b64fa03abf715524236809925a0704ada1f
Rachel Moser <moser.rachel@gmail.com>
2020-03-04 16:41:57 -0600

    logic for one loop through

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,10 +1,17 @@
 def bubble_sort array
     i = 0
     while i < (array.length - 1)
+        puts array[i]
+        if array[i] > array[i + 1]
+            array[i], array[i + 1] = array[i + 1], array[i]
+        end
         i += 1
+        print array
+        puts ""
     end
     array
+    puts ""
 end
 
-puts bubble_sort([4,3,78,2,0,2])
+print bubble_sort([4,3,78,2,0,2])
 # => [0,2,2,3,4,78]
