51f73aa8eaa657832fe3daf96095ad846f5dba7a
Rachel Moser <moser.rachel@gmail.com>
2020-03-05 11:29:47 -0600

    finalized logic for 2 bubble_sorts

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -9,24 +9,34 @@ def bubble_sort array
                 unsorted = true
             end
             i += 1
-            print array
-            puts ""
         end
     end
+    print array    
 end
 
-# def bubble_sort array
-#     i = 0
-#     while i < (array.length - 1)
-#         puts array[i]
-#         array[i], array[i + 1] = array[i + 1], array[i] if array[i] > array[i + 1]
-#         i += 1
-#         print array
-#         puts ""
-#     end
-#     array
-#     puts ""
-# end
-
-print bubble_sort([4,3,78,2,0,2])
+bubble_sort([4,3,78,2,0,2])
+puts ""
 # => [0,2,2,3,4,78]
+
+def bubble_sort_by array
+    unsorted = true
+    while unsorted do
+        i = 0
+        unsorted = false
+        while i < (array.length - 1)
+            if (yield array[i], array[i + 1]).to_i >= 0
+                array[i], array[i + 1] = array[i + 1], array[i]
+                unsorted = true
+            end
+            i += 1
+        end
+    end
+    print array
+end
+
+
+bubble_sort_by(["hi","hello","hey"]) do |left,right|
+    left.length - right.length
+end
+puts ""
+# => ["hi", "hey", "hello"]
