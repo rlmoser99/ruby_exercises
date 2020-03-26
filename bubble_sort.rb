bb8c4b2192f3f8d449fd9ec7e6e7914167e4acbd
Rachel Moser <moser.rachel@gmail.com>
2020-03-26 11:12:17 -0500

    added explanations

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -24,7 +24,9 @@ def bubble_sort_by array
         i = 0
         unsorted = false
         while i < (array.length - 1)
+            # If statement that yields to the block of code to calculate the difference of left.length & right.length
             if (yield array[i], array[i + 1]).to_i >= 0
+                # If difference >= 0, then second word is shorter and needs to switch places
                 array[i], array[i + 1] = array[i + 1], array[i]
                 unsorted = true
             end
@@ -34,9 +36,43 @@ def bubble_sort_by array
     print array
 end
 
-
 bubble_sort_by(["hi","hello","hey"]) do |left,right|
     left.length - right.length
 end
 puts ""
 # => ["hi", "hey", "hello"]
+
+def bubble_sort_by_explanation array
+    puts "The beginning order of the array: #{array}"
+    puts ""
+    unsorted = true
+    while unsorted do
+        i = 0
+        unsorted = false
+        while i < (array.length - 1)
+            puts "Yield to calculate the difference of #{array[i]}.length - #{array[i + 1]}.length"
+            difference = (yield array[i], array[i + 1]).to_i
+            puts "The difference is #{difference}."
+            puts "If is < 0 the order second word is longer and nothing changes"
+            if (difference >= 0)
+                puts "BUT IF THE DIFFERENCE >= 0 the order second word is shorter & needs to be switched!"
+                puts "This next line switches the words #{array[i]} and #{array[i + 1]}"
+                array[i], array[i + 1] = array[i + 1], array[i]
+                unsorted = true
+            end
+            i += 1
+            puts "#{array} is the current array in while loop"
+            puts ""
+        end
+    end
+    puts "The ending order of the array: #{array}"
+end
+
+puts ""
+puts "EXPLANATION:"
+bubble_sort_by_explanation(["labrador", "pug", "beagle"]) do |left,right|
+    puts "Calculate the difference of #{left}.length - #{right}.length"
+    left.length - right.length
+end
+puts ""
+# => ["pug", "beagle", "labrador"]
